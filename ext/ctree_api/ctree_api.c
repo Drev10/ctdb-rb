940e76b07cf7ed290e29545476fc0d2fc907fbb6
Daniel Johnston <daniel.johnston@adfitech.com>
2012-04-12 08:08:21 -0500

    Implemented CtreeField class.

--- ext/ctree_api/ctree_api.c
+++ ext/ctree_api/ctree_api.c
@@ -6,7 +6,12 @@
 #define CTREE_PASS "YBUR"
 
 
+#define GetCtreeStruct(obj) (Check_Type(obj, T_DATA), (struct ctree*)DATA_PTR(obj))
+#define GetHandler(obj)     (Check_Type(obj, T_DATA), &(((struct ctree*)DATA_PTR(obj))->handler))
+
+
 VALUE cCtree;
+VALUE cCtreeField;
 
 struct ctree {
     CTHANDLE handler;
@@ -33,7 +38,7 @@ static VALUE init(VALUE klass)
     return obj;
 }
 
-static VALUE logon(int argc, VALUE* argv, VALUE klass)
+static VALUE connect(int argc, VALUE* argv, VALUE klass)
 {
     VALUE host, username, password, database;
     char *h, *u, *p, *d;
@@ -47,7 +52,16 @@ static VALUE logon(int argc, VALUE* argv, VALUE klass)
     // d = NILorSTRING(database);
 
     obj = Data_Make_Struct(klass, struct ctree, 0, free_ctree, ctp);
-    ctdbLogon(&ctp->handler, CTREE_ENGINE, CTREE_USER, CTREE_PASS);
+    // ctdbLogon(&ctp->handler, CTREE_ENGINE, CTREE_USER, CTREE_PASS);
+    ctdbConnect(&ctp->handler, CTREE_ENGINE);
+}
+
+static VALUE disconnect(VALUE obj)
+{
+    CTHANDLE* cth = GetHandler(obj);
+    ctdbDisconnect(cth);
+    GetCtreeStruct(obj)->connection = Qfalse;
+    return obj;
 }
 
 static VALUE initialize(int argc, VALUE* argv, VALUE obj)
@@ -58,7 +72,17 @@ static VALUE initialize(int argc, VALUE* argv, VALUE obj)
 void Init_ctree_api(void)
 {
     cCtree = rb_define_class("Ctree", rb_cObject);
+    cCtreeField = rb_define_class_under(cCtree, "Field", rb_cObject);
+
+    // class methods
     rb_define_singleton_method(cCtree, "init", init, 0);
-    rb_define_singleton_method(cCtree, "logon", logon, -1);
+    rb_define_singleton_method(cCtree, "connect", connect, -1);
+    // instance methods
+    rb_define_method(cCtree, "disconnect", disconnect, 0);
     rb_define_method(cCtree, "initiallize", initialize, -1);
+
+    // rb_define_method(cCtreeField, "name", field_name, 0);
+    // rb_define_method(cCtreeField, "table", field_table, 0);
+    // rb_define_method(cCtreeField, "type", field_type, 0);
+    // rb_define_method(cCtreeField, "length", field_length, 0);
 }
