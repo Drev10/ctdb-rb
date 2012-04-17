24cba0b9908a9b9e2d6cb02907a0ae92614792d3
Daniel Johnston <daniel.johnston@adfitech.com>
2012-04-17 14:12:23 -0500

    Switched ctree to a module

--- lib/ctree.rb
+++ lib/ctree.rb
@@ -1,9 +1,9 @@
 begin
   require 'ctree_sdk'
 rescue LoadError
-  warn "WARNING: ctree_api not loaded"
+  warn "WARNING: ctree_sdk not loaded."
 end
 
-class Ctree
+module Ctree
   Version = VERSION = '1.0.0'
 end
