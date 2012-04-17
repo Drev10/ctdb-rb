11028974ccb081ad5ed725e7e8523ccd21bdcfc0
Daniel Johnston <daniel.johnston@adfitech.com>
2012-04-17 14:12:48 -0500

    Added tests for session, table, and record objects.

--- test/test_ctree.rb
+++ test/test_ctree.rb
@@ -1,16 +1,46 @@
-# $:.unshift(File.dirname(__FILE__) + '/../lib')
-# require 'ctree'
 require 'test/unit'
-require 'turn/autorun'
+require 'turn'
+
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'ctree'
 
 class TestCtree < Test::Unit::TestCase
-  
+
   def setup
+    @table_name = "custmast"
+    @c = { host: "FAIRCOMS", username: "", password: "" }
   end
-  
-  def test_init
-    assert_nothing_raised{ @ct = Ctree.init }
-    # assert_nothing_raised{ @ct.logout }
+
+  def test_session
+    assert_nothing_raised { @session = Ctree::Session.new }
+    assert_equal(false, @session.active?)
+    assert_nothing_raised { @session.logon(@c[:host], @c[:username], @c[:password]) }
+    assert(@session.active?)
+    assert_nothing_raised { @session.logout }
+    assert_equal(false, @session.active?)
   end
-  
+
+  def test_table
+    assert_nothing_raised do 
+      @session = Ctree::Session.new
+      @session.logon(@c[:host], @c[:username], @c[:password])
+    end
+    assert_nothing_raised { @table = Ctree::Table.new(@session) }
+    assert_nil(@table.path)
+    # assert_nil(@table.name)
+    assert_nothing_raised { @table.open(@table_name) }
+    assert_equal(@table_name, @table.name)
+    assert_nothing_raised { @table.close }
+    assert_nothing_raised { @session.logout }
+  end
+
+  def test_record
+    assert_nothing_raised do 
+      @session = Ctree::Session.new
+      @session.logon(@c[:host], @c[:username], @c[:password])
+      @table = Ctree::Table.new(@session)
+    end
+
+  end
+
 end
