78f4ba8c71f8afe98a3dd9962be18586cf7bbacf
Daniel Johnston <daniel.johnston@adfitech.com>
2012-05-01 14:38:18 -0500

    Switch module name.

--- test/test_ctree.rb
+++ test/test_ctree.rb
@@ -14,7 +14,7 @@ class TestCtree < Test::Unit::TestCase
   end
 
   def test_session
-    assert_nothing_raised { @session = Ctree::Session.new }
+    assert_nothing_raised { @session = CTDB::Session.new }
     assert_equal(false, @session.active?)
     assert_raise(ArgumentError){ @session.logon }
     assert_nothing_raised { @session.logon(@c[:host], @c[:username], @c[:password]) }
@@ -25,20 +25,20 @@ class TestCtree < Test::Unit::TestCase
 
   def test_session_locking
     assert_nothing_raised do
-      @session = Ctree::Session.new
+      @session = CTDB::Session.new
       @session.logon(@c[:host], @c[:username], @c[:password])
     end
-    assert(@session.lock(Ctree::LOCK_WRITE))
+    assert(@session.lock(CTDB::LOCK_WRITE))
     assert(@session.locked?)
     assert(@session.unlock)
     assert_equal(false, @session.locked?)
-    assert_nothing_raised{ @session.lock!(Ctree::LOCK_WRITE) }
-    # assert_raise(Ctree::Error){ @session.lock!(Ctree::LOCK_READ) }
+    assert_nothing_raised{ @session.lock!(CTDB::LOCK_WRITE) }
+    # assert_raise(CTDB::Error){ @session.lock!(CTDB::LOCK_READ) }
     assert_nothing_raised{ @session.unlock! }
   end
 
   def test_session_path_prefix
-    assert_nothing_raised { @session = Ctree::Session.new }
+    assert_nothing_raised { @session = CTDB::Session.new }
     assert_nil(@session.path_prefix)
     assert_nothing_raised { @session.path_prefix = "/foo/bar" }
     assert_equal("/foo/bar", @session.path_prefix)
@@ -50,10 +50,10 @@ class TestCtree < Test::Unit::TestCase
 
   def test_table
     assert_nothing_raised do
-      @session = Ctree::Session.new
+      @session = CTDB::Session.new
       @session.logon(@c[:host], @c[:username], @c[:password])
     end
-    assert_nothing_raised { @table = Ctree::Table.new(@session) }
+    assert_nothing_raised { @table = CTDB::Table.new(@session) }
     assert_nil(@table.path)
     assert_nothing_raised { @table.path = @table_path }
     assert_equal(@table_path, @table.path)
@@ -67,13 +67,13 @@ class TestCtree < Test::Unit::TestCase
 
   def test_record
     assert_nothing_raised do 
-      @session = Ctree::Session.new
+      @session = CTDB::Session.new
       @session.logon(@c[:host], @c[:username], @c[:password])
-      @table = Ctree::Table.new(@session)
+      @table = CTDB::Table.new(@session)
       @table.path = @table_path
       @table.open(@table_name)
     end
-    assert_nothing_raised { @record = Ctree::Record.new(@table) }
+    assert_nothing_raised { @record = CTDB::Record.new(@table) }
 
     assert_nothing_raised{ @record.first }
     # assert_equal("foo", @record.field('t_fstring'))
@@ -96,14 +96,14 @@ class TestCtree < Test::Unit::TestCase
 
   def test_record_getters_and_setters
     assert_nothing_raised do 
-      @session = Ctree::Session.new
+      @session = CTDB::Session.new
       @session.logon(@c[:host], @c[:username], @c[:password])
-      @table = Ctree::Table.new(@session)
+      @table = CTDB::Table.new(@session)
       @table.path = @table_path
       @table.open(@table_name)
     end
     assert_nothing_raised do
-      @record = Ctree::Record.new(@table)
+      @record = CTDB::Record.new(@table)
       @record.first
     end
     assert_equal("foo", @record.get_field('t_fstring'))
@@ -116,13 +116,13 @@ class TestCtree < Test::Unit::TestCase
 
   def test_record_indexes
     assert_nothing_raised do 
-      @session = Ctree::Session.new
+      @session = CTDB::Session.new
       @session.logon(@c[:host], @c[:username], @c[:password])
-      @table = Ctree::Table.new(@session)
+      @table = CTDB::Table.new(@session)
       @table.path = @table_path
       @table.open(@table_name)
     end
-    assert_nothing_raised { @record = Ctree::Record.new(@table) }
+    assert_nothing_raised { @record = CTDB::Record.new(@table) }
     # assert_nothing_raised{ @record.default_index = @index }
     # assert_equal(@index, @record.default_index)
     # assert_nothing_raised{ @record.default_index = :"#{@index}" }
