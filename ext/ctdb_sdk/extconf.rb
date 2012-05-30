1c8645ae66e3b5b5a86f2ac50d07db6a992cbcaa
Daniel Johnston <daniel.johnston@adfitech.com>
2012-05-30 16:36:07 -0500

    Decided to go with multithreaded dynamic libs for all.

--- ext/ctdb_sdk/extconf.rb
+++ ext/ctdb_sdk/extconf.rb
@@ -19,26 +19,20 @@ EOS
   exit
 end
 
-if osx?
-  api_dir = "ctree.ctdb/multithreaded/static"
-  lib     = "mtclient"
-elsif linux?
-  api_dir = "ctree.ctdb/singlethreaded/static"
-  lib     = "ctclient"
-end
-
-base_dir = with_config('faircom-dir')
-inc_dirs = [ "#{base_dir}/include", "#{base_dir}/include/sdk/#{api_dir}" ]
-# cflags   = "-g -m64"
-lib_dir  = "#{base_dir}/lib/#{api_dir}"
+ct_dir = with_config('faircom-dir')
+api_dir  = %w(ctree.ctdb multithreaded dynamic).join(File::SEPARATOR)
+lib      = "mtclient"
+lib_dir  = File.join(ct_dir, 'lib', api_dir)
+inc_dirs = [ File.join(ct_dir, 'include', 'sdk', api_dir),
+             File.join(ct_dir, 'include') ]
 
 inc_dirs.each do |path|
   $INCFLAGS << " -I#{path}"
 end
-$LIBPATH << "#{lib_dir}"
+$LIBPATH  << "#{lib_dir}"
 
 errors = []
-errors << "'ctdbsdk'" unless have_header('ctdbsdk.hpp')
+errors << "'ctdbsdk.h'" unless have_header('ctdbsdk.h')
 errors << "'#{lib}'"  unless have_library(lib)
 fail("missing dependencies: #{errors.join(',')}") unless errors.empty?
 
